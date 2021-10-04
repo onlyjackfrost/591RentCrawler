5a29c2632f874d8afd4a4dbc700ce6f9c3ca4d2b
Andy Yen <andy.yen@onedegree.hk>
2021-10-04 11:28:30 +0800

    rm env

--- src/env.py
+++
@@ -1,3 +0,0 @@
-const_channel_access_token = "TsugEnxgi3TOTIgTEfumO7nF9ehlCenCoqF/du7wvQS+QSkaV1nbRqeJsoAj6BGtp2ETJFOjRawiGXyCXqJKTP5aUtabhze7G0K6eWkbMyRkuau9sYqK9hOpPG+ht6OXylWLw4QmDNVoZoBcckfS+wdB04t89/1O/w1cDnyilFU="
-
-const_channel_secret = "642bf0bf9db4b025a09780281b114722"
