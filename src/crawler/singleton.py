3e311c9fb6cc37114bf11906630e5462e5673804
onlyjackfrost <onlyjackfrost@gmail.com>
2021-10-16 00:35:19 +0800

    tmp

--- src/crawler/singleton.py
+++ src/crawler/singleton.py
@@ -1,16 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 session = requests.session()
 
-def getCsrfToken(url_591='https://rent.591.com.tw/'):
+
+def getCsrfToken(url_591='https://rent.591.com.tw/?region=1'):
+    headers = {'User-Agent': 'Custom'}
     bs = BeautifulSoup(
-        session.get(url_591, cookies={
-            'urlJumpIp': '1'
-        }).text, 'html.parser')
+        session.get(url_591, headers=headers).text, 'html.parser')
     tag = bs.html.head.find('meta', {'name': 'csrf-token'})
     token = tag.attrs['content']
     return token
+
 
 csrf_Token = getCsrfToken()  #global variable
