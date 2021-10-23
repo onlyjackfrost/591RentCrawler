825d69443b58ef710059cbd5e296165ee8105aaf
onlyjackfrost <onlyjackfrost@gmail.com>
2021-10-23 18:28:16 +0800

    print log once

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -2,12 +2,11 @@ import os
 import time
 
 is_local = os.getenv('LOCAL')
+if not is_local:
+    print('is not local')
 
 
 def logCrawlProgress(info):
-    if not is_local:
-        print('is not local')
-        return
     timeString = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime())
     with open('GetId.log', 'at') as file:
         file.write(timeString + '  ' + info)
