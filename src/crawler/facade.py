25400e2fc54e6d8f7217238f665a05e9ab5ac32d
onlyjackfrost <onlyjackfrost@gmail.com>
2022-02-13 01:59:59 +0800

    new taipei

--- src/crawler/facade.py
+++ src/crawler/facade.py
@@ -1,3 +1,4 @@
+from click.decorators import option
 from crawler.crawler import getFullHouseList
 import copy
 
@@ -8,15 +9,11 @@ newtaipei_region_code = 3
 def basicQueryString():
     queryString = {
         'type': 1,
-        #  'searchtype': 1,
-        'role': 1,  #屋主刊登
-        # 'multiPrice': ['10000_20000', '20000_30000'],  #租金
-        'multiPrice': '10000_20000,20000_30000',  #租金
         'orderType': 'desc',  # 最新
         'multiNotice': 'not_cover',  # 排除頂加
         'not_cover': 1,
         'totalRows': 90,
-        'shType': 'host'
+        'shType': 'host',
     }
     return copy.deepcopy(queryString)
 
@@ -24,7 +21,6 @@ def basicQueryString():
 def QueryStringPrice(option, price):
     prices = {10000: '10000_20000', 20000: '20000_30000'}
     option['multiPrice'] = prices[price]
-    return option
 
 
 def crawTaipeiHouseList(filter_func=None):
@@ -44,3 +40,40 @@ def crawTaipeiHouseList(filter_func=None):
     houseList4 = getFullHouseList(taipei_region_code, option2, filter_func)
 
     return houseList1 + houseList2 + houseList3 + houseList4
+
+
+def crawNewTaipeiLocationsHouseList(filter_func=None, locations=[]):
+    def query_by_location(filter_func=None, location=0):
+        house_list = []
+        option = basicQueryString()
+        # option['region'] = 3
+        option['section'] = location  # 位置
+        option['searchtype'] = 1  # 猜測是跟section同時出現的query string
+        #整層住家 & 1~2w 2~3w
+        option['kind'] = 1
+        QueryStringPrice(option, 10000)
+        houses = getFullHouseList(newtaipei_region_code, option, filter_func)
+        house_list += houses
+        QueryStringPrice(option, 20000)
+        houses = getFullHouseList(newtaipei_region_code, option, filter_func)
+        house_list += houses
+
+        option['kind'] = 2  #獨立套房
+        option['other'] = 'lift'  #有電梯
+        QueryStringPrice(option, 10000)
+        houses = getFullHouseList(newtaipei_region_code, option, filter_func)
+        house_list += houses
+        QueryStringPrice(option, 20000)
+        houses = getFullHouseList(newtaipei_region_code, option, filter_func)
+        house_list += houses
+        return house_list
+
+    house_list = []
+    for location in locations:
+        try:
+            houses = query_by_location(filter_func=filter_func,
+                                       location=location)
+            house_list += houses
+        except:
+            continue
+    return house_list
